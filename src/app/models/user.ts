14d61465dc4dcd88a9125742ee717138d085835f
renestimu <byumitsiz@gmail.com>
2022-08-23 11:45:12 +0300

    master

---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export interface User{
+  $key: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+
+}
---
+++ src/app/users/users.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddUsersComponent } from './add-users/add-users.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddUsersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
