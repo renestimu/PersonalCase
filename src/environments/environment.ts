e233d1c28ed069aaab385783868babd598a0cdf8
renestimu <byumitsiz@gmail.com>
2022-08-24 00:50:40 +0300

    duzenleme

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+      apiKey: "xxxxxx-xxxxxx_xxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxx_xxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxx_xxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxx_xxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxx_xxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+    appId: "1:xxxxxxxxxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxx"
+  }
+};
